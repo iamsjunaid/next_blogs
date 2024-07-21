4c2fdba7c2d0258b7a653922f74f3ec8d1a558f8
Junaid Syed <sjunaid626@gmail.com>
2024-07-21 21:12:21 +0530

    Add dummy featured posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -2,11 +2,46 @@ import { Fragment } from "react";
 import Hero from "../../components/home-page/hero";
 import FeaturedPosts from "../../components/home-page/featured-posts";
 
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a React framework that provides features like server-side rendering, API routes, file-based routing, and more!",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs1",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a React framework that provides features like server-side rendering, API routes, file-based routing, and more!",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a React framework that provides features like server-side rendering, API routes, file-based routing, and more!",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a React framework that provides features like server-side rendering, API routes, file-based routing, and more!",
+    date: "2022-02-10",
+  },
+];
+
 function AllPostsPage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts />
+      <FeaturedPosts posts={DUMMY_POSTS}/>
     </Fragment>
   );
 }
