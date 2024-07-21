ea36d5baf6e40547c363897de41a2ba1a0949f14
Junaid Syed <sjunaid626@gmail.com>
2024-07-21 20:31:53 +0530

    Complete navigation and layout

---
+++ components/layout/logo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./logo.module.css";
+
+function Logo() {
+  return <div className={classes.logo}>Junaid' Next Blog</div>;
+}
+
+export default Logo;
