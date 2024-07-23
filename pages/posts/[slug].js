e827496ae2039fd1b15b4718d36e140b31c8ae79
Junaid Syed <sjunaid626@gmail.com>
2024-07-23 13:49:47 +0530

    use markdown for posts

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,34 @@
 import PostContent from "./post-detail/post-content";
+import { getPostData, getPostsFiles} from "../../lib/posts-util";
 
-function PostDetailPage() {
-  return <PostContent />;
+function PostDetailPage(props) {
+  const post = props.slug;
+  return <PostContent post={post} />;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      slug: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export async function getStaticPaths() {
+  const fileNames = getPostsFiles();
+  
+  const slugs = fileNames.map((fileName) => fileName.replace(/\.md$/, ""));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug } })),
+    fallback: false,
+  };
 }
 
 export default PostDetailPage;
