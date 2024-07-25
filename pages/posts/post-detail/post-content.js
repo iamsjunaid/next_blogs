57bff62dc3a6385a1c6aa909189dd2c3f09949a8
Junaid Syed <sjunaid626@gmail.com>
2024-07-25 17:05:45 +0530

    Fix import issue in production

--- pages/posts/post-detail/post-content.js
+++ pages/posts/post-detail/post-content.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
 import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
 import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
 import ReactMarkdown from "react-markdown";
